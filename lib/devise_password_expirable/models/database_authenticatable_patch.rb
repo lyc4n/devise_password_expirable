4a6215e143b055201e8c395fb6ddfc58e8e4a676
jenjaina <jenni.kissinger@gmail.com>
2014-11-12 14:00:29 -0500

    Devise patch will match devise version 1.1.3

--- lib/devise_password_expirable/models/database_authenticatable_patch.rb
+++ lib/devise_password_expirable/models/database_authenticatable_patch.rb
@@ -1,20 +1,19 @@
 module Devise
   module Models
     module DatabaseAuthenticatablePatch
-      def update_with_password(params, *options)
+      def update_with_password(params={})
         current_password = params.delete(:current_password)
 
         new_password = params[:password]
         new_password_confirmation = params[:password_confirmation]
 
         result = if valid_password?(current_password) && new_password.present? && new_password_confirmation.present?
-          update_attributes(params, *options)
+          update_attributes(params)
         else
-          self.assign_attributes(params, *options)
-          self.valid?
           self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
           self.errors.add(:password, new_password.blank? ? :blank : :invalid)
           self.errors.add(:password_confirmation, new_password_confirmation.blank? ? :blank : :invalid)
+          self.attributes = params
           false
         end
 
